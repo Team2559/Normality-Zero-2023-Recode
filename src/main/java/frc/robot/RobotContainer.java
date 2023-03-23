0c624c9322201e96ce27835ab868804ecdab4cfc
LoganJVedder <normalityzero@gmail.com>
2023-03-22 21:03:26 -0400

    Import of Swerve Drive Specialties SwerveLib

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,89 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import frc.robot.Constants.OperatorConstants;
+import frc.robot.commands.Autos;
+import frc.robot.commands.ExampleCommand;
+import frc.robot.subsystems.ExampleSubsystem;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+
+import frc.robot.commands.DriveCommand;
+import frc.robot.subsystems.DrivetrainSubsystem;
+
+public class RobotContainer {
+  private final ExampleSubsystem myExampleSubsystem = new ExampleSubsystem();
+  
+  private final DrivetrainSubsystem drivetrain = new DrivetrainSubsystem();
+
+  private final CommandXboxController m_driverController = new CommandXboxController(OperatorConstants.kDriverControllerPort);
+
+  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  public RobotContainer() {
+    // Configure the trigger bindings
+    configureBindings();
+    drivetrain.register();
+
+    drivetrain.setDefaultCommand(new DriveCommand(drivetrain,
+            () -> -modifyAxis(m_driverController.getLeftY()), // Axes are flipped here on purpose
+            () -> -modifyAxis(m_driverController.getLeftX()),
+            () -> -modifyAxis(m_driverController.getRightX())
+    ));
+
+    // new Button(controller::getBackButtonPressed)
+    //        .whenPressed(drivetrain::zeroGyroscope);
+  }
+
+  public DrivetrainSubsystem getDrivetrain() {
+    return drivetrain;
+  }
+
+  private static double deadband(double value, double deadband) {
+    if (Math.abs(value) > deadband) {
+        if (value > 0.0) {
+            return (value - deadband) / (1.0 - deadband);
+        } else {
+            return (value + deadband) / (1.0 - deadband);
+        }
+    } else {
+        return 0.0;
+    }
+}
+
+private static double modifyAxis(double value) {
+    // Deadband
+    value = deadband(value, 0.05);
+
+    // Square the axis
+    value = Math.copySign(value * value, value);
+
+    return value;
+}
+
+/**
+   * Use this method to define your trigger->command mappings. Triggers can be created via the
+   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
+   * predicate, or via the named factories in {@link
+   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
+   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
+   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
+   * joysticks}.
+   */
+  private void configureBindings() {
+
+  }
+
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+    // An example command will be run in autonomous
+     return Autos.exampleAuto(myExampleSubsystem);
+  }
+}
