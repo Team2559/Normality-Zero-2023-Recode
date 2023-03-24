e0eca24598e7aef8da1e18595a7437bc35dfe106
Normality-Zero <normalityzero@gmail.com>
2023-03-23 22:41:55 -0400

    Encoder offsets
    Updated the encoder offsets.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -14,7 +14,7 @@ package frc.robot;
  */
 public final class Constants {
   public static class OperatorConstants {
-    public static final int kDriverControllerPort = 0;
+    public static final int kDriverControllerPort = 2;
   }
   public static final double DRIVETRAIN_TRACKWIDTH_METERS = 0.2794;
   public static final double DRIVETRAIN_WHEELBASE_METERS = 0.2794;
@@ -24,22 +24,22 @@ public final class Constants {
   public static final int FRONT_LEFT_MODULE_DRIVE_MOTOR = 7;
   public static final int FRONT_LEFT_MODULE_STEER_MOTOR = 8;
   public static final int FRONT_LEFT_MODULE_STEER_ENCODER = 13;
-  public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(34.54);
+  public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(280.0);
 
   public static final int FRONT_RIGHT_MODULE_DRIVE_MOTOR = 1;
   public static final int FRONT_RIGHT_MODULE_STEER_MOTOR = 2;
   public static final int FRONT_RIGHT_MODULE_STEER_ENCODER = 10;
-  public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(118.52);
+  public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(65.0);
 
   public static final int BACK_LEFT_MODULE_DRIVE_MOTOR = 5;
   public static final int BACK_LEFT_MODULE_STEER_MOTOR = 6;
   public static final int BACK_LEFT_MODULE_STEER_ENCODER = 12;
-  public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(71.99);
+  public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(65.0);
 
   public static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 3;
   public static final int BACK_RIGHT_MODULE_STEER_MOTOR = 4;
   public static final int BACK_RIGHT_MODULE_STEER_ENCODER = 11;
-  public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(261.71);
+  public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(165.0);
 
   //These are the shooter can IDs for the robot
   public static final int LEFT_SHOOTER = 15;
